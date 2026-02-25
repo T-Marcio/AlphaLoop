408891dc36985dfa677e6fae3e21616bc0652478
Marcio Terán <marcioteran7@outlook.com>
2026-02-25 13:45:02 -0300

    añadí postbuild en package.json para crear docs/.nojekyll para github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Rutas relativas para que funcione en GitHub Pages (carpeta `docs`)
+  base: './',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'docs'
+  }
 })
