815492c8e17d00031779636c58b94b38ec05beed
Marcio Terán <marcioteran7@outlook.com>
2026-02-27 11:51:44 -0300

    acualizo copiando header y footer para github

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/AlphaLoop/', //compatible con github y netlify
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/components/header.html', dest: ''}, //copia a dist
+        { src: 'src/components/footer.html', dest: ''}
+      ]
+    })
   ],
   build: {
-    outDir: 'dist', //dist estandar
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
