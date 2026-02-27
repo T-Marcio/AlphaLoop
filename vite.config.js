d9e6f031b42028af389731823da4c79d5e9bc331
Marcio Terán <marcioteran7@outlook.com>
2026-02-27 16:05:26 -0300

    actualizando los errores del menú para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
@@ -10,11 +11,24 @@ export default defineConfig({
       targets: [
         { src: 'src/components/header.html', dest: ''}, //copia a dist
         { src: 'src/components/footer.html', dest: ''}
+       /* { src: 'src/pages/portfolio.html', dest: ''},
+        { src: 'src/pages/pricing.html', dest: ''},
+        { src: 'src/pages/blog.html', dest: ''},
+        { src: 'src/pages/contact.html', dest: ''} */
       ]
     })
   ],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        portfolio: resolve(__dirname, 'src/pages/portfolio.html'),
+        pricing: resolve(__dirname, 'src/pages/pricing.html'),
+        blog: resolve(__dirname, 'src/pages/blog.html'),
+        contact: resolve(__dirname, 'src/pages/contact.html'),
+      }
+    }
   }
 })
