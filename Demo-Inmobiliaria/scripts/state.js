b3305818c53db41f19d5cf2626c0b0be42a2d184
Marcio Terán <marcioteran7@outlook.com>
2026-03-01 18:55:03 -0300

    mantener Demo-Ecommerce en esta rama

--- Demo-Inmobiliaria/scripts/state.js
+++
@@ -1,9 +0,0 @@
-export const state = {
-  properties: [],              // Todas las propiedades
-  filteredProperties: [],      // Propiedades que cumplen los filtros
-  filters: {                   // Filtros aplicados
-    type: '',
-    beds: '',
-    maxPrice: Infinity
-  }
-};
