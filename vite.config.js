b1939083db5f20898ea9eef4fee65a011c787214
Marcio Terán <marcioteran7@outlook.com>
2026-03-03 14:22:17 -0300

    build AlphaLoop para github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/AlphaLoop/', //nombre del proyecto en GitHub Pages
   plugins: [
     tailwindcss(),
   ],
