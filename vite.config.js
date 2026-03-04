75763cb7a5bdec612e6f38971ed2bd77739284c3
Marcio Terán <marcioteran7@outlook.com>
2026-03-04 10:47:38 -0300

    no sirvio enrutar desde import resolve para el menú de header y footer. Porque los archivos desde public se generaron duplicados en dos rutas diferentes

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,19 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+//import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/AlphaLoop/', //nombre del proyecto en GitHub Pages
   build: {
     outDir: 'docs', //carpeta de salida para GitHub Pages
-    rollupOptions: {
+    /*rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         portfolio: resolve(__dirname, 'public/portfolio.html'),
-        pricing: resolve(__dirname, 'public/pricing.html'),
-        blog: resolve(__dirname, 'public/blog.html'),
-        contact: resolve(__dirname, 'public/contact.html'),
-        articulo1: resolve(__dirname, 'public/articulo1.html'),
-        articulo2: resolve(__dirname, 'public/articulo2.html'),
-        articulo3: resolve(__dirname, 'public/articulo3.html'),
-        articulo4: resolve(__dirname, 'public/articulo4.html'),
       },
-    },
+    },*/
   },
   plugins: [
     tailwindcss(),
-  ],
+  ]
 })
