765538d21be0a5c519fd07525e412dbb925f2c53
Marcio Terán <marcioteran7@outlook.com>
2026-03-17 18:29:16 -0300

    deshago cambios en el menu hamburguesa de demo-inmobiliaría

--- public/demos/demo-inmobiliaria/scripts/navbar.js
+++ public/demos/demo-inmobiliaria/scripts/navbar.js
@@ -8,6 +8,3 @@ export function initNavbar(root) {
   });
 }
 
-document.getElementById("menuBtn").addEventListener("click", function() {
-  document.getElementById("sidebar").classList.toggle("active");
-});
